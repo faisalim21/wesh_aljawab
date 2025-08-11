2a37902d5674c584f458b0b0fbd33fb012a3b497
Faisal <faisal@example.com>
2025-08-12 01:41:02 +0300

    56

--- wesh_aljawab/asgi.py
+++ wesh_aljawab/asgi.py
@@ -2,27 +2,19 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import OriginValidator
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wesh_aljawab.settings')
 
 django_asgi_app = get_asgi_application()
 
-from games import routing
-
-ALLOWED_ORIGINS = [
-    "https://wesh-aljawab.onrender.com",
-    "http://wesh-aljawab.onrender.com",
-    "http://127.0.0.1:8000",
-    "http://localhost:8000",
-]
+from games import routing  # بعد تهيئة Django
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": OriginValidator(
+    "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(routing.websocket_urlpatterns)
-        ),
-        ALLOWED_ORIGINS
+        )
     ),
 })
