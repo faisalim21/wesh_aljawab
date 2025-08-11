0a5b171063b5203b7b19c515b3b5b5a8c4b41c18
Faisal <faisal@example.com>
2025-08-12 01:32:12 +0300

    55

--- wesh_aljawab/asgi.py
+++ wesh_aljawab/asgi.py
@@ -1,11 +1,11 @@
+import os
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import OriginValidator
-from django.core.asgi import get_asgi_application
-from django.conf import settings
-import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wesh_aljawab.settings')
+
 django_asgi_app = get_asgi_application()
 
 from games import routing
