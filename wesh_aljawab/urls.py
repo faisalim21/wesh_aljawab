b1603329dc9f5d09ceaad5e68dce1c7abb5317a4
Faisal <faisal@example.com>
2025-09-11 22:46:02 +0300

    192

--- wesh_aljawab/urls.py
+++ wesh_aljawab/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 def home_view(request):
     """الصفحة الرئيسية"""
