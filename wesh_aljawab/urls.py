f2ccce2a17012bafc398f758053629becb18f345
Faisal <faisal@example.com>
2025-11-26 21:04:58 +0300

    ا

--- wesh_aljawab/urls.py
+++ wesh_aljawab/urls.py
@@ -7,8 +7,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 def home_view(request):
-    """الصفحة الرئيسية"""
-    return render(request, 'home.html')
+    return render(request, 'base.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
